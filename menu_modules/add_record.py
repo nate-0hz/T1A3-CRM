922787882d0c6d01116c5ffbca34c3a598112a75
Nate Picone <hello@natepicone.com>
2023-04-29 09:46:43 +1000

    added folder for menu_modules. Committing ahead of refactor branch

---
+++ menu_modules/add_record.py
@@ -0,0 +1,15 @@
+class Entry:
+    def __init(self, name, phone, company, email, birthday, last_contact, next_contact):
+        self.name = name
+        self.phone = phone
+        self.company = company
+        self.email = email
+        self.birthday = birthday
+        self.last_contact = last_contact
+        self.next_contact = next_contact
+
+print("Add a contact.")
+input_name = input("What is the name to add? ")
+#####
+# enter function to check if name exists
+#####
